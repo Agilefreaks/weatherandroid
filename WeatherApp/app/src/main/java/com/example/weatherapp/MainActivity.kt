027ec38cb515a9807b07da2f15d38b1216fb7abb
Orasan Corina <62331686+corinaorasan171015@users.noreply.github.com>
2021-01-14 12:38:37 +0200

    Small requested changes
    Formated code
    Fixed location show on the first app
    Used coarse location

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -14,28 +14,28 @@ import com.google.android.gms.location.LocationServices
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private var requestValue: Int=1
+    private var requestValue: Int = 1
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        title="Weather"
+        title = "Weather"
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         requestLocationPermission()
         getLocation()
     }
 
-
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
                                             grantResults: IntArray) {
         when (requestCode) {
-            requestValue-> {
+            requestValue -> {
                 if (grantResults.isNotEmpty() && grantResults[0] ==
                         PackageManager.PERMISSION_GRANTED) {
                     if ((ContextCompat.checkSelfPermission(this,
                                     Manifest.permission.ACCESS_FINE_LOCATION) ==
                                     PackageManager.PERMISSION_GRANTED)) {
                         Toast.makeText(this, resources.getString(R.string.permission_granted), Toast.LENGTH_SHORT).show()
+                        getLocation()
                     }
                 } else {
                     Toast.makeText(this, resources.getString(R.string.permission_denied), Toast.LENGTH_SHORT).show()
@@ -45,46 +45,44 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun requestLocationPermission(){
-        val shouldShowRequestPermission=ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
-            if (shouldShowRequestPermission){
+    fun requestLocationPermission() {
+        val shouldShowRequestPermission = ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (shouldShowRequestPermission) {
                 ActivityCompat.requestPermissions(this,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), requestValue)
-            }
-            else
-            {
+                        arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), requestValue)
+            } else {
                 ActivityCompat.requestPermissions(this,
-                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), requestValue)
+                        arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), requestValue)
             }
         }
     }
-    fun getLocation(){
+
+    fun getLocation() {
         if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
         ) {
-                requestLocationPermission()
+            requestLocationPermission()
             return
         }
         fusedLocationClient.lastLocation
-            .addOnSuccessListener { location : Location? ->
-                val textView: TextView = findViewById(R.id.coordinatesTextView)
-                textView.setText(resources.getString(R.string.location_coordinates,location?.latitude, location?.longitude))
-                if(location!=null){
-                    val geoCoder=Geocoder(this, Locale.getDefault())
-                    val addresses: List<Address>
-                    //Code  bellow might work on physical device but throws error on emulator
-                    addresses= geoCoder.getFromLocation(location.latitude, location.longitude,1)
-                    val result = addresses[0].subAdminArea
-                    val locationTextView: TextView = findViewById(R.id.locationTextView)
-                    locationTextView.setText(result)
+                .addOnSuccessListener { location: Location? ->
+                    val textView: TextView = findViewById(R.id.coordinatesTextView)
+                    textView.setText(resources.getString(R.string.location_coordinates, location?.latitude, location?.longitude))
+                    if (location != null) {
+                        val geoCoder = Geocoder(this, Locale.getDefault())
+                        val addresses: List<Address>
+                        addresses = geoCoder.getFromLocation(location.latitude, location.longitude, 1)
+                        val result = addresses[0].subAdminArea
+                        val locationTextView: TextView = findViewById(R.id.locationTextView)
+                        locationTextView.setText(result)
+                    }
                 }
-            }
     }
 
 }
